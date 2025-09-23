08bc90ff887830a00984bcdc69005260c8782887
saprykinzxc <saprykinoff@inbox.ru>
2025-09-23 21:06:17 +0000

    solve binpower

--- multiplication/multiplication.h
+++ multiplication/multiplication.h
@@ -3,5 +3,5 @@
 #include <stdexcept>
 
 int64_t Multiply(int a, int b) {
-∏    return static_cast<int64_t>(a) * static_cast<int64_t>(b);
+    return static_cast<int64_t>(a) * static_cast<int64_t>(b);
 }
