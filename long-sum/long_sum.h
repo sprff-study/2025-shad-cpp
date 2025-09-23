958cb80c386dae38490724247bccd4b549c98fc1
saprykinzxc <saprykinoff@inbox.ru>
2025-09-23 21:03:39 +0000

    fix long sum

--- long-sum/long_sum.h
+++ long-sum/long_sum.h
@@ -10,10 +10,10 @@ std::string LongSum(const std::string& a, const std::string& b) {
     for (int i = 0; i <= n; ++i) {
         int ac = 0, bc = 0;
         if (i < static_cast<int>(a.size())) {
-            ac = a[a.size() - 1 - i];
+            ac = a[a.size() - 1 - i] - '0';
         }
         if (i < static_cast<int>(b.size())) {
-            bc = b[b.size() - 1 - i];
+            bc = b[b.size() - 1 - i] - '0';
         }
         int x = ac + bc + over;
         res += '0' + x % 10;
@@ -24,7 +24,7 @@ std::string LongSum(const std::string& a, const std::string& b) {
         res.pop_back();
     }
 
-    for (int l = 0, r = res.size() - 1; l < r; l++, r++) {
+    for (int l = 0, r = static_cast<int>(res.size()) - 1; l < r; l++, r--) {
         std::swap(res[l], res[r]);
     }
 
--- multiplication/multiplication.h
+++ multiplication/multiplication.h
@@ -3,5 +3,5 @@
 #include <stdexcept>
 
 int64_t Multiply(int a, int b) {
-    return static_cast<int64_t>(a) * static_cast<int64_t>(b);
+∏    return static_cast<int64_t>(a) * static_cast<int64_t>(b);
 }
