306ae3c580dfedc6b255549238a1192cb1198e3f
saprykinzxc <saprykinoff@inbox.ru>
2025-09-23 14:56:29 +0000

    solve binpower

--- bin-pow/binpow.h
+++ bin-pow/binpow.h
@@ -3,5 +3,14 @@
 #include <stdexcept>
 
 int BinPow(int a, int64_t b, int c) {
-    throw std::runtime_error{"Not implemented"};
+    if (b == 0) {
+        return 1;
+    }
+    if (b % 2 == 0) {
+        int64_t z = static_cast<int64_t>(BinPow(a, b / 2, c));
+        return z * z % c;
+    }
+    int64_t z = static_cast<int64_t>(BinPow(a, b - 1, c));
+    return z * a % c;
 }
+
