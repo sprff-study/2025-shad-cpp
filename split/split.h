3f3c3cba32111b35a2e4f578a9c347b5d7a78aaa
saprykinzxc <saprykinoff@inbox.ru>
2025-10-03 06:19:12 +0000

    split

--- split/split.h
+++ split/split.h
@@ -5,5 +5,18 @@
 #include <vector>
 
 std::vector<std::string> Split(const std::string& string, const std::string& delimiter = " ") {
-    
+    if (string.empty()) {
+        return {};
+    }
+    size_t pos = 0;
+    std::vector<std::string> res;
+    while (true) {
+        size_t next = string.find(delimiter, pos);
+        res.push_back(string.substr(pos, next - pos));
+        if (next == std::string::npos) {
+            break;
+        }
+        pos = next + delimiter.size();
+    }
+    return res;
 }
