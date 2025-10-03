b26488d87c189169aa73b9df3b890b7962690d5b
saprykinzxc <saprykinoff@inbox.ru>
2025-10-03 19:51:37 +0000

    static-map

--- static-map/static_map.h
+++ static-map/static_map.h
@@ -3,13 +3,27 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <algorithm>
 
 class StaticMap {
 public:
-    explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items) {
+    explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items)
+        : data_(items) {
+        std::sort(data_.begin(), data_.end());
     }
 
     bool Find(const std::string& key, std::string* value) const {
-        return false;
+        auto i = std::lower_bound(data_.begin(), data_.end(), make_pair(key, ""));
+        if (i == data_.end()) {
+            return false;
+        }
+        if (i->first != key) {
+            return false;
+        }
+        *value = i->second;
+        return true;
     }
+
+private:
+    std::vector<std::pair<std::string, std::string>> data_;
 };
