63f327e14bdd0531018e361430e7f570a19ad73c
saprykinzxc <saprykinoff@inbox.ru>
2025-10-03 12:30:26 +0000

    stack

--- stack/stack.h
+++ stack/stack.h
@@ -1,25 +1,34 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class Stack {
 public:
     void Push(int x) {
+        a.push_back(x);
     }
 
     bool Pop() {
+        if (Empty()) {
+            return false;
+        }
+        a.pop_back();
         return true;
     }
 
     int Top() const {
-        return 0;
+        return a.back();
     }
 
     bool Empty() const {
-        return false;
+        return a.empty();
     }
 
     size_t Size() const {
-        return 0;
+        return a.size();
     }
+
+private:
+    std::vector<int> a;
 };
