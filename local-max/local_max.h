b228210debb10f11e22a135156b4565759970628
saprykinzxc <saprykinoff@inbox.ru>
2025-10-18 19:26:41 +0000

    local max

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -4,5 +4,30 @@
 
 template <class Iterator>
 Iterator LocalMax(Iterator first, Iterator last) {
+    Iterator cur = first;
+    while (cur != last) {
+        auto nxt = cur;
+        ++nxt;
+        bool l = (cur == first);
+        bool r = (nxt == last);
+        if (!l) {
+            auto prv = cur;
+            prv--;
+            if (*prv < *cur) {
+                l = true;
+            }
+        }
+        if (!r) {
+            if (*nxt < *cur) {
+                r = true;
+            }
+        }
+        if (l && r) {
+            return cur;
+        }
+        ++cur;
+    }
+    return last;
+
     throw std::runtime_error{"Not implemented"};
 }
