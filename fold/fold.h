5685451a397a8b62b763b81e34339b644232b3f3
saprykinzxc <saprykinoff@inbox.ru>
2025-10-18 19:19:38 +0000

    fold

--- fold/fold.h
+++ fold/fold.h
@@ -1,13 +1,51 @@
 #pragma once
+#include <cstddef>
 
-struct Sum {};
 
-struct Prod {};
+struct Sum {
+    template <typename T>
+    T operator()(T a, T b) {
+        return a + b;
+    }
+};
 
-struct Concat {};
+struct Prod {
+    template <typename T>
+    T operator()(T a, T b) {
+        return a * b;
+    }
+};
 
-class Length {};
+struct Concat {
+    template <typename T>
+    T operator()(T a, T b) {
+        for (auto el : b) {
+            a.push_back(el);
+        }
+        return a;
+    }
+};
+
+class Length {
+    public:
+    Length(size_t *cnt) : cnt_(cnt) {
+    }
+    template <typename T>
+    T operator()(T a, T _) {
+        (*cnt_)++;
+        return a;
+    }
+
+private:
+    size_t *cnt_;
+};
 
 template <class Iterator, class T, class BinaryOp>
 T Fold(Iterator first, Iterator last, T init, BinaryOp func) {
+    T res = init;
+    while (first != last) {
+        res = func(res, *first);
+        ++first;
+    }
+    return res;
 }
