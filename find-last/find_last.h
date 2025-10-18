2223c79f32da095b2229b3668e666b450cdb6977
saprykinzxc <saprykinoff@inbox.ru>
2025-10-18 19:10:44 +0000

    find_last reverse order

--- find-last/find_last.h
+++ find-last/find_last.h
@@ -4,12 +4,12 @@
 
 template <class Iterator, class T>
 Iterator FindLast(Iterator first, Iterator last, const T& value) {
-    Iterator ans = last;
-    while (first != last) {
-        if (*first == value) {
-            ans = first;
+    Iterator back = last;
+    while (first != back) {
+        --back;
+        if (*back == value) {
+            return back;
         }
-        ++first;
     }
-    return ans;
+    return last;
 }
