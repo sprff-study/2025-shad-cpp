f7f95fa5914fc68c1840e1eec743160ed384a8de
saprykinzxc <saprykinoff@inbox.ru>
2025-10-23 18:49:58 +0000

    uvy

--- raytracer-geom/vector.h
+++ raytracer-geom/vector.h
@@ -2,16 +2,34 @@
 
 #include <array>
 #include <cstddef>
+#include <cmath>
 
 class Vector {
 public:
-    Vector();
-    Vector(double x, double y, double z);
+    Vector() {
+        data_[0] = 0;
+        data_[1] = 0;
+        data_[2] = 0;
+    }
+    Vector(double x, double y, double z) {
+        data_[0] = x;
+        data_[1] = y;
+        data_[2] = z;
+    }
 
-    double& operator[](size_t ind);
-    double operator[](size_t ind) const;
+    double& operator[](size_t ind) {
+        return data_[ind];
+    }
+    double operator[](size_t ind) const {
+        return data_[ind];
+    }
 
-    void Normalize();
+    void Normalize() {
+        double sz = sqrt(data_[0] * data_[0] + data_[1] * data_[1] + data_[2] * data_[2]);
+        data_[0] /= sz;
+        data_[1] /= sz;
+        data_[2] /= sz;
+    }
 
 private:
     std::array<double, 3> data_;
